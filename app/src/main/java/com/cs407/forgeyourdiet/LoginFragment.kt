fad2c972b97df818ba2bc6131100b0b6fc363512
Minhan Tang <1900574002@qq.com>
2024-10-27 18:12:31 -0500

    setup navigation

---
+++ app/src/main/java/com/cs407/forgeyourdiet/LoginFragment.kt
@@ -0,0 +1,18 @@
+package com.cs407.forgeyourdiet
+
+import android.content.Context
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+class LoginFragment: Fragment()  {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?,
+    ): View {
+        val view = inflater.inflate(R.layout.login_fragment, container, false)
+        return view
+    }
+}
