2c4b0b88998beed97c0e8fd43e463b0294c32355
Minhan Tang <1900574002@qq.com>
2024-12-06 17:55:41 -0600

    nutrition summary logic done

--- app/src/main/java/com/cs407/forgeyourdiet/NutritionSummaryFragment.kt
+++ app/src/main/java/com/cs407/forgeyourdiet/NutritionSummaryFragment.kt
@@ -5,9 +5,13 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
+import android.widget.RadioGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.findNavController
 import com.cs407.forgeyourdiet.data.UserStatus
 import com.cs407.forgeyourdiet.data.UserStatusRepository
 import org.w3c.dom.Text
@@ -20,7 +24,14 @@ class NutritionSummaryFragment: Fragment() {
     private lateinit var carbohydratesBox: androidx.cardview.widget.CardView
     private lateinit var fatBox: androidx.cardview.widget.CardView
     private lateinit var username: String
+    private lateinit var logEntryTitle: TextView
+    private lateinit var logEntryValue: EditText
 
+    private var selectedNutrient: NutrientType = NutrientType.CALORIES
+
+    private enum class NutrientType {
+        NONE, CALORIES, PROTEIN, CARBOHYDRATES, FAT
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -46,21 +57,73 @@ class NutritionSummaryFragment: Fragment() {
         carbohydratesBox = view.findViewById(R.id.carbohydratesBox)
         fatBox = view.findViewById(R.id.fatBox)
 
+        logEntryTitle = view.findViewById(R.id.logCaloriesSection)
+        logEntryValue = view.findViewById(R.id.caloriesEditText)
+
         titleImplement()
         // Observe ViewModel for data changes
         // TODO: change logic of observation to cache or other observation
         nutritionViewModel.userStatus.observe(viewLifecycleOwner) { userStatus ->
             if (userStatus != null) {
-                Log.i("good", "good user status")
                 updateUI(userStatus)
             }
-            else{
-                Log.i("e", "null user status")
+        }
+
+        caloriesBox.setOnClickListener { onNutrientSelected(NutrientType.CALORIES) }
+        proteinBox.setOnClickListener { onNutrientSelected(NutrientType.PROTEIN) }
+        carbohydratesBox.setOnClickListener { onNutrientSelected(NutrientType.CARBOHYDRATES) }
+        fatBox.setOnClickListener { onNutrientSelected(NutrientType.FAT) }
+
+        val macronutrientRadioGroup = view.findViewById<RadioGroup>(R.id.macronutrientRadioGroup)
+
+        macronutrientRadioGroup.setOnCheckedChangeListener { _, checkedId ->
+            when (checkedId) {
+                R.id.caloriesRadioButton -> onNutrientSelected(NutrientType.CALORIES)
+                R.id.proteinRadioButton -> onNutrientSelected(NutrientType.PROTEIN)
+                R.id.carbsRadioButton -> onNutrientSelected(NutrientType.CARBOHYDRATES)
+                R.id.fatRadioButton -> onNutrientSelected(NutrientType.FAT)
             }
         }
-        nutritionViewModel.loadUserStatus(userViewModel.userState.value.username)
+
+        // reset button logic
+        view.findViewById<View>(R.id.resetButton).setOnClickListener{
+            logEntryValue.text = null
+        }
+
+        view.findViewById<View>(R.id.addFoodButton).setOnClickListener{
+            findNavController().navigate(R.id.action_nutritionSummaryFragment_to_cleanYourFridgeFragment)
+        }
+
+        // save button logic
+        view.findViewById<View>(R.id.saveButton).setOnClickListener {
+            val enteredValue = logEntryValue.text.toString().toIntOrNull()
+            if (enteredValue != null) {
+                updateSelectedNutrient(enteredValue)
+            } else {
+                Log.e("LogEntry", "Invalid number entered")
+            }
+        }
     }
 
+    private fun onNutrientSelected(nutrientType: NutrientType) {
+        selectedNutrient = nutrientType
+        when (nutrientType) {
+            NutrientType.CALORIES -> logEntryTitle.text = getString(R.string.NutritionSummary_log_calories)
+            NutrientType.PROTEIN -> logEntryTitle.text = getString(R.string.NutritionSummary_log_protein)
+            NutrientType.CARBOHYDRATES -> logEntryTitle.text = getString(R.string.NutritionSummary_log_carbs)
+            NutrientType.FAT -> logEntryTitle.text = getString(R.string.NutritionSummary_log_fat)
+            else -> logEntryTitle.text = ""
+        }
+    }
+    private fun updateSelectedNutrient(value: Int) {
+        when (selectedNutrient) {
+            NutrientType.CALORIES -> nutritionViewModel.updateCaloriesGoal(username, value)
+            NutrientType.PROTEIN -> nutritionViewModel.updateProteinGoal(username, value)
+            NutrientType.CARBOHYDRATES -> nutritionViewModel.updateCarbsGoal(username, value)
+            NutrientType.FAT -> nutritionViewModel.updateFatGoal(username, value)
+            else -> Log.e("LogEntry", "No nutrient selected")
+        }
+    }
     private fun titleImplement(){
         caloriesBox.findViewById<TextView>(R.id.boxTitle).text = getString(R.string.calories)
         proteinBox.findViewById<TextView>(R.id.boxTitle).text = getString(R.string.protein)
